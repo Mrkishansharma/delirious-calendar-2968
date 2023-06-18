5601dcec3a4ab8c228c3c61d96becf96051c0ff2
Chandan2812 <chandanpms@gmail.com>
2023-06-18 12:14:36 +0530

    stylist table

--- Frontend/Scripts/admin.stylist.js
+++ Frontend/Scripts/admin.stylist.js
@@ -7,13 +7,41 @@ const adminGetStylist=()=>{
     })
     .then((res)=> res.json())
     .then((data)=>{
-        console.log(data)
-        //renderAllStylist(data.data)
+        console.log(data.data)
+        renderAllStylist(data.data)
     })
     .catch(err=>console.log(err))
 }
 adminGetStylist();
 
+
+let container=document.getElementById("adminStylist")
+    function renderAllStylist(data){
+        
+        container.innerHTML=""
+        data.forEach(element=>{
+            let tr=document.createElement("tr")
+
+            let td1=document.createElement("td")
+            let img=document.createElement("img")
+            img.src=element.image
+
+            let td2=document.createElement("td")
+            td2.textContent=element.name
+
+            let td3=document.createElement("td")
+            td3.textContent=element.gender
+            let td4=document.createElement("td")
+            td4.textContent=element.speciality1+", "+element.speciality2+", "+element.speciality3
+           
+            
+           td1.append(img)
+           tr.append(td1,td2,td3,td4)
+            container.append(tr)
+        })
+    }
+
+    
 
 
 
